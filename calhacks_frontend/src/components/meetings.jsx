b8d6c944511171641236927d516bdeadc97dbfaa
kavlekar101 <kavlekar101@gmail.com>
2023-06-17 15:01:26 -0700

    ez card component for the meetings

--- calhacks_frontend/src/components/meetings.jsx
+++ calhacks_frontend/src/components/meetings.jsx
@@ -1,12 +1,52 @@
 import * as React from 'react';
 import { Card } from '@mui/material';
 
+/*
+    -------------------------------------------------
+    Today's Meetings
+    -------------------------------------------------
+    9:30am - 10:30am | CS 61A Discussion
+    11:00am - 12:00pm | CS 61A Lecture
+    1:00pm - 2:00pm | CS 61A Lab
+    2:00pm - 3:00pm | CS 61A Office Hours
+    -------------------------------------------------
+    Tomorrow's Meetings
+    -------------------------------------------------
+    9:30am - 10:30am | CS 61A Discussion
+    11:00am - 12:00pm | CS 61A Lecture
+    1:00pm - 2:00pm | CS 61A Lab
+    2:00pm - 3:00pm | CS 61A Office Hours
+    -------------------------------------------------
+    This Week's Meetings
+    -------------------------------------------------
+    9:30am - 10:30am | CS 61A Discussion
+    11:00am - 12:00pm | CS 61A Lecture
+    1:00pm - 2:00pm | CS 61A Lab
+    2:00pm - 3:00pm | CS 61A Office Hours
+    -------------------------------------------------
+*/
 
 // size is the size of the component that we are trying to display
 export default function meetings(size) {
-    return () => {
-        <div>
-            <Card sx={{ minWidth: 275 }}/>
-        </div>
+    return (props) => {
+        <Card sx={{ minWidth: 275 * size }}>
+            <CardContent>
+                <Typography variant="h5" component="div">
+                    Today's Meetings
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    9:30am - 10:30am | CS 61A Discussion
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    11:00am - 12:00pm | CS 61A Lecture
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    1:00pm - 2:00pm | CS 61A Lab
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    2:00pm - 3:00pm | CS 61A Office Hours
+                </Typography>
+            </CardContent>
+        </Card>
     }
 }
