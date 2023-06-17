02f227b551c412bfd4c951a8d4966045727b643d
kavlekar101 <kavlekar101@gmail.com>
2023-06-17 15:33:29 -0700

    fixed the json formatting for the meetings

--- calhacks_frontend/src/components/meetings.jsx
+++ calhacks_frontend/src/components/meetings.jsx
@@ -31,25 +31,14 @@ import { Card, CardContent, Typography } from '@mui/material';
 export default function meetings(size) {
     return (props) => {
         return (
-            <Card sx={{ minWidth: 275 * size }}>
-                <CardContent>
-                    <Typography variant="h5" component="div">
-                        Today's Meetings
-                    </Typography>
-                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                        9:30am - 10:30am | CS 61A Discussion
-                    </Typography>
-                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                        11:00am - 12:00pm | CS 61A Lecture
-                    </Typography>
-                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                        1:00pm - 2:00pm | CS 61A Lab
-                    </Typography>
-                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                        2:00pm - 3:00pm | CS 61A Office Hours
-                    </Typography>
-                </CardContent>
-            </Card>
+          <Card sx={{ width: "100%", height: "100%" }}>
+            <Typography sx={{ fontSize: 14 }} color="text.primary">
+              Today's Meetings
+            </Typography>
+            <Typography sx={{ fontSize: 14 }} color="text.secondary">
+              9:30am - 10:30am | CS 61A Discussion
+            </Typography>
+          </Card>
         );
     }
 }
