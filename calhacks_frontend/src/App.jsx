10ac93950fcaee609998d809ca2ecc3cf032652f
d0rb <hacperu2010@gmail.com>
2023-06-17 15:00:39 -0700

    added chatgpt layout support

--- calhacks_frontend/src/App.jsx
+++ calhacks_frontend/src/App.jsx
@@ -7,7 +7,7 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { green, purple, orange } from '@mui/material/colors';
 import pullData from './util/pullData';
 import { summarize, getWidgets, stripWidgetSize } from './util/summarize';
-import reply_email from './components/reply_email';
+import email_reply from './components/email_reply';
 import meetings from './components/meetings';
 import './App.css'
 import { Configuration, OpenAIApi } from "openai";
@@ -21,8 +21,8 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const WIDGET_MAPPINGS = {
-  reply_email: reply_email,
-  meetings: meetings,
+  email_reply,
+  meetings,
 }
 
 for (const [widget, component] of Object.entries(WIDGET_MAPPINGS)) {
@@ -56,38 +56,46 @@ function App() {
       const data = await pullData();
       const [briefingSummary, widgets] = await Promise.all([summarize(data), getWidgets(data)]);
 
-      const layoutSuggestion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo-0613",
-        messages: [
-          {"role": "system", "content": "You are an assistant that designs widget layouts for a service that presents daily briefings to users based on their emails, calendar events, and more. Based on the user's data (emails, calendar events, etc.), you design a widget layout based on the most important items and things the user should know. You have access to a function to present the widgets that you call by passing the list of widgets. The order of the widgets is sorted by importance, with the most important widget first. You can also define whether to emphasize the widget or to make it large. Only include each widget type once, including variants. Do not include a widget and its large version at the same time. Each widget's normal version takes up 1 space, their large versions take up 2 spaces, and their xlarge versions take up 3 spaces. The maximum number of spaces is 12."},
-          {"role": "user", "content": `Here is a summary of everything I should know:\n${briefingSummary}\n\nLay out my daily briefing.`},
-        ],
-        functions: [
-          {
-            "function": "layout",
-            "description": "Lay out the daily briefing.",
-            "parameters": {
-              "type": "array",
-              "items": {
-                "type": "string",
-                "enum": widgets,
-              },
-            },
-          }
-        ],
-        function_call: {
-          "name": "layout",
-        },
-      });
+      // const layoutSuggestion = await openai.createChatCompletion({
+      //   model: "gpt-3.5-turbo-0613",
+      //   messages: [
+      //     {"role": "system", "content": "You are an assistant that designs widget layouts for a service that presents daily briefings to users based on their emails, calendar events, and more. Based on the user's data (emails, calendar events, etc.), you design a widget layout based on the most important items and things the user should know. You have access to a function to present the widgets that you call by passing the list of widgets. The order of the widgets is sorted by importance, with the most important widget first. You can also define whether to emphasize the widget or to make it large; a larger widget for more important items is probably a good idea. Only include each widget type once, including variants. Do not include a widget and its large version at the same time. Each widget's normal version takes up 1 space, their large versions take up 2 spaces, and their xlarge versions take up 3 spaces. The maximum number of spaces is 12."},
+      //     {"role": "user", "content": `Here is a summary of everything I should know:\n${briefingSummary}\n\nLay out my daily briefing.`},
+      //   ],
+      //   functions: [
+      //     {
+      //       name: "layout",
+      //       description: "Lay out the daily briefing.",
+      //       parameters: {
+      //         type: "object",
+      //         properties: {
+      //           layout: {
+      //             type: "array",
+      //             items: {
+      //               type: "string",
+      //               enum: widgets,
+      //             },
+      //             description: "The ordered list of widgets to display."
+      //           }
+      //         }
+      //       },
+      //     }
+      //   ],
+      //   function_call: {
+      //     "name": "layout",
+      //   },
+      // });
 
-      const layout = layoutSuggestion.choices[0].message.function_call.arguments;
+      // const layout = JSON.parse(layoutSuggestion.data.choices[0].message.function_call.arguments).layout;
+      const layout = ["email_reply_large", "meetings"];
       console.log(layout);
 
       const widgetComponents = layout.map((widget) => {
         const WidgetComponent = WIDGET_MAPPINGS[widget];
+        const [widgetName, size] = stripWidgetSize(widget);
         const component = (
-          <Grid item xs={size * 4}>
-            <WidgetComponent { ...data[stripWidgetSize(widget)] } />
+          <Grid item xs={size * 4} key={widget}>
+            <WidgetComponent { ...data[widgetName] } />
           </Grid>
         );
 
