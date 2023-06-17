46cb43e2c62c85f53b6c30c7475211096f16aef8
d0rb <hacperu2010@gmail.com>
2023-06-17 15:29:21 -0700

    using theming in card styling

--- calhacks_frontend/src/components/email_reply.jsx
+++ calhacks_frontend/src/components/email_reply.jsx
@@ -2,12 +2,15 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Typography from '@mui/material/Typography';
+import { useTheme } from '@mui/material/styles';
 
 
 export default function email_reply(size) {
     return (props) => {
+        const theme = useTheme();
+        console.log(theme);
         return (
-            <Card sx={{ width: '100%', height: '100%' }}>
+            <Card sx={{ width: '100%', height: '100%' }} elevation={6}>
                 <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
                     Word of the Day
                 </Typography>
