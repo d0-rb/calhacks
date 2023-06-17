6bf274f183a7adbdeffa72793870ef580061305a
d0rb <hacperu2010@gmail.com>
2023-06-17 16:36:34 -0700

    weird bug fix

--- calhacks_frontend/src/App.jsx
+++ calhacks_frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from 'react'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { green, purple, orange } from '@mui/material/colors';
 import pullData from './util/pullData';
 import { summarize, getWidgets, stripWidgetSize } from './util/summarize';
