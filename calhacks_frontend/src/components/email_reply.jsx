a8a88db6f6ec2ec90f9912fcbb6fc83403701dc2
d0rb <hacperu2010@gmail.com>
2023-06-17 16:34:45 -0700

    working on reply email widget

--- calhacks_frontend/src/components/email_reply.jsx
+++ calhacks_frontend/src/components/email_reply.jsx
@@ -4,6 +4,10 @@ import CardActions from '@mui/material/CardActions';
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useTheme } from '@mui/material/styles';
 
 
@@ -30,8 +34,16 @@ function EmailCards({ data, numEmails }) {
         )
     }
 
+    if (emailCards.length < numEmails) {
+        return (
+            <Stack direction="row" spacing={1} sx={{ width: `calc(100% * ${data.length/numEmails})` }}>
+                {emailCards}
+            </Stack>
+        );
+    }
+
     return (
-        <Stack direction="row" spacing={1} sx={{ width: `calc(100% * ${data.length/numEmails})` }}>
+        <Stack direction="row" spacing={0} sx={{ width: `calc(100% * ${data.length/numEmails})` }}>
             {emailCards}
         </Stack>
     );
@@ -39,9 +51,26 @@ function EmailCards({ data, numEmails }) {
 
 
 function EmailCard({ from, subject, body, summary, suggested_response }) {
+    const theme = useTheme();
+    // left align text
     return (
-        <Box sx={{ height: '100%', width: '100%' }}>
-            {from}
+        <Box sx={{ height: '90%', width: '90%', padding: '5%' }}>
+            <Typography variant="h5" align="left" noWrap={true}>
+                {subject}
+            </Typography>
+            <Typography variant="body2" align="left" noWrap={true} gutterBottom sx={{ color: theme.palette.text.secondary }}>
+                {from}
+            </Typography>
+            <Paper align="left" elevation={3}>
+                <Stack direction="row">
+                    <Typography sx={{ padding: '5%' }} variant="body2" align="left" noWrap={true}>
+                        {summary}
+                    </Typography>
+                    <IconButton aria-label="expand">
+                        <ExpandMoreIcon />
+                    </IconButton>
+                </Stack>
+            </Paper>
         </Box>
     )
 }
