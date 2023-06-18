92beba1f545f68b5fcc3f1f0ed88ca76984927a4
d0rb <hacperu2010@gmail.com>
2023-06-18 09:45:13 -0700

    new mock

--- calhacks_frontend/src/App.jsx
+++ calhacks_frontend/src/App.jsx
@@ -71,7 +71,7 @@ function App() {
       // const layoutSuggestion = await openai.createChatCompletion({
       //   model: "gpt-3.5-turbo-0613",
       //   messages: [
-      //     {"role": "system", "content": "You are an assistant that designs widget layouts for a service that presents daily briefings to users based on their emails, calendar events, and more. Based on the user's data (emails, calendar events, etc.), you design a widget layout based on the most important items and things the user should know. You have access to a function to present the widgets that you call by passing the list of widgets. The order of the widgets is sorted by importance, with the most important widget first. You can also define the size of a widget; a larger widget for more important items is probably a good idea. Only include each widget type once, including variants. Do not include a widget and its large version at the same time, though not all widgets have to be included. Each widget's normal version takes up 1 space, their large versions take up 2 spaces, and their xlarge versions take up 3 spaces. The maximum number of spaces is 8, though if you use 6 or less then it will be presented in a smaller form. Try not to use too many xlarges unless it is very important, as they take up a lot of space."},
+      //     {"role": "system", "content": "You are an assistant that designs widget layouts for a service that presents daily briefings to users based on their emails, calendar events, and more. Based on the user's data (emails, calendar events, etc.), you design a widget layout based on the most important items and things the user should know. You have access to a function to present the widgets that you call by passing the list of widgets. The order of the widgets is sorted by importance, with the most important widget first. You can also define the size of a widget; a larger widget for more important items is probably a good idea. Only include each widget type once, including variants. Do not include a widget and its large version at the same time, though not all widgets have to be included. Each widget's normal version takes up 1 space, their large versions take up 2 spaces, and their xlarge versions take up 3 spaces. The maximum number of spaces is 8, though if you use 6 or less then it will be presented in a smaller form. Try not to use too many xlarges unless it is very important, as they take up a lot of space. Also, try to leave 1 or 2 spaces for the chatbot, 2 is better, but 1 is fine."},
       //     {"role": "user", "content": `Here is a summary of everything I should know:\n${briefingSummary}\n\nLay out my daily briefing.`},
       //   ],
       //   functions: [
@@ -99,7 +99,7 @@ function App() {
       // });
 
       // const rawLayout = JSON.parse(layoutSuggestion.data.choices[0].message.function_call.arguments).layout;
-      const rawLayout = ["email_reply", "finance", "meetings", "travel", "shopping"];
+      const rawLayout = ["email_reply", "social_media", "AWS", "shopping"];
       let layout = [];
 
       let totalSpace = 0;
