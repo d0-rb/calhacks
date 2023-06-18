92ff943066a407f0ee4e83ec340c3b016d6f35a0
d0rb <hacperu2010@gmail.com>
2023-06-18 14:22:15 -0700

    adding clenda suppor !

--- calhacks_frontend/src/util/pullData.js
+++ calhacks_frontend/src/util/pullData.js
@@ -2,6 +2,62 @@ import { mock1, mock2 } from './mocks';
 
 // pull all data needed for briefing from api
 
-export default async function pullData() {
-    return mock2;
+export default async function pullData(openai) {
+    let data = mockFragment2;
+    let categories = {};
+    let calendar = {};
+
+    try {
+        // const response = await fetch('http://localhost:8000/categories');
+        // categories = await response.json();
+
+        // for (const [category, categoryData] of Object.entries(categories)) {
+        //     if (categoryData?.emails?.length > 0) {
+        //         categoryData.emails.forEach((email, i) => {
+        //             openai.createChatCompletion({
+        //                 model: "gpt-3.5-turbo-0613",
+        //                 messages: [
+        //                     {"role": "system", "content": "You are an assistant summarizes emails to provide daily briefings."},
+        //                     {"role": "user", "content": `Here is a summary of an email:\nFrom: ${email.from}\nSubject: ${email.subject}\nDate: ${new Date(email.date * 1000).toLocaleDateString('en-US', { weekday: 'long', month: 'long', day: 'numeric' })}\nBody: ${email.body}\n\nPlease summarize in around 5 words.`},
+        //                 ],
+        //             }).then((response) => {
+        //                 const summary = response.data.choices[0].message.content.trim();
+                        
+        //                 categoryData.emails[i].summary = summary;
+        //             });
+        //         })
+                
+        //         // categoryData.emails[i].summary = 'Sample Summary';
+        //     } else {
+        //         delete categories[category];
+        //     }
+        // }
+
+        categories = mock2;
+    } catch (error) {
+        console.warn(error);
+    }
+
+    try {
+        const response = await fetch('http://localhost:8000/calendar');
+        calendar = await response.json();
+
+        calendar = {
+            meetings: calendar,
+        }
+    } catch (error) {
+        console.warn(error);
+    }
+
+    // update data with categories
+    data = {
+        ...data,
+        ...categories,
+        ...calendar,
+    };
+
+    // calendar ?!
+    
+
+    return data;
 }
